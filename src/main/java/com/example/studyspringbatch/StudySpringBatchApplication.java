186af069a9e0464ce8e69bef8915f2c2d5ca093f
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-08-07 20:14:49 +0900

    first commit

---
+++ src/main/java/com/example/studyspringbatch/StudySpringBatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.studyspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBatchApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(SpringApplication.run(StudySpringBatchApplication.class, args)));
+	}
+}
